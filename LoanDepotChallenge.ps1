c93b9d2a7b012025064009a8510ed4c8897037f5
MichaelZ35 <32824438+MichaelZ35@users.noreply.github.com>
2020-11-19 14:05:57 -0500

    Add files via upload

---
+++ LoanDepotChallenge.ps1
@@ -0,0 +1,19 @@
+﻿function FindRepeatedChars([String] $inputStr) {
+    $inputArray = $inputStr.ToCharArray()
+    #we use a hash table so we only need to loop through at most once (O(n))
+    $foundChars = @{}
+
+    #we use foreach so we can break when we find the first repeated char and avoid unnecessary work
+    foreach ($char in $inputArray) {
+        if($foundChars.ContainsKey($char)) {
+            #Write-Output "$($char) is the first repeated char"
+            return $char
+        }        
+        $foundChars.Add($char, 'found')
+    }
+
+    #I'm returning a blank if no matching chars found
+    return ''
+}
+
+FindRepeatedChars -inputStr abcdedcba
